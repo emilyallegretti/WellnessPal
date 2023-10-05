16e4a8394a0e607585c4e8714fa7a18c8467e454
emilyallegretti <emmy.allegretti@gmail.com>
2023-10-05 12:43:31 -0400

    merge with annas branch

--- app/src/main/java/com/bignerdranch/android/wellnesspal/ui/petinfo/PetInfoFragment.kt
+++ app/src/main/java/com/bignerdranch/android/wellnesspal/ui/petinfo/PetInfoFragment.kt
@@ -1,7 +1,9 @@
 package com.bignerdranch.android.wellnesspal.ui.petinfo
 
+import android.content.Context
 import androidx.lifecycle.ViewModelProvider
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
@@ -9,6 +11,7 @@ import android.view.ViewGroup
 import android.widget.TextView
 import com.bignerdranch.android.wellnesspal.databinding.FragmentPetInfoBinding
 
+private const val TAG = "PetInfoFragment"
 class PetInfoFragment : Fragment() {
 
     private var _binding: FragmentPetInfoBinding? = null
@@ -32,12 +35,59 @@ class PetInfoFragment : Fragment() {
         petInfoViewModel.text.observe(viewLifecycleOwner) {
             textView.text = it
         }
+        Log.d(TAG, "OnCreateView() called")
         return root
     }
 
     override fun onDestroyView() {
+        Log.d(TAG, "OnDestroyView() called")
         super.onDestroyView()
         _binding = null
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        Log.d(TAG, "OnViewCreated() called")
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        Log.d(TAG, "OnCreate() called")
+    }
+
+    override fun onStart() {
+        super.onStart()
+        Log.d(TAG, "OnStart() called")
+    }
+
+    override fun onResume() {
+        super.onResume()
+        Log.d(TAG, "OnResume() called")
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        Log.d(TAG, "OnDestroy() called")
+    }
+
+    override fun onAttach(context: Context) {
+        super.onAttach(context)
+        Log.d(TAG, "OnAttach() called")
+    }
+
+    override fun onDetach() {
+        super.onDetach()
+        Log.d(TAG, "OnDetach() called")
+    }
+
+    override fun onPause() {
+        super.onPause()
+        Log.d(TAG, "OnPause() called")
+    }
+
+    override fun onStop() {
+        super.onStop()
+        Log.d(TAG, "OnStop() called")
     }
 
 }
--- app/src/main/java/com/bignerdranch/android/wellnesspal/ui/petinfo/PetInfoViewModel.kt
+++ app/src/main/java/com/bignerdranch/android/wellnesspal/ui/petinfo/PetInfoViewModel.kt
@@ -6,7 +6,7 @@ import androidx.lifecycle.ViewModel
 
 class PetInfoViewModel : ViewModel() {
     private val _text = MutableLiveData<String>().apply {
-        value = "This is PetInfo Fragment"
+        value = "This is Pet Fragment"
     }
     val text: LiveData<String> = _text
 }
