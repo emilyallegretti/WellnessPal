dca336af2d3bda1d954f3d040b3638e88b646873
Emma Ryan <ryan.1778@buckeyemail.osu.edu>
2023-10-19 17:39:31 -0400

    Added profilePage viewModel and dataRepository changes

--- app/src/main/java/com/bignerdranch/android/wellnesspal/DataRepository.kt
+++ app/src/main/java/com/bignerdranch/android/wellnesspal/DataRepository.kt
@@ -4,7 +4,13 @@ import android.content.Context
 import android.util.Log
 import com.bignerdranch.android.wellnesspal.models.Goal
 import com.bignerdranch.android.wellnesspal.models.User
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.ValueEventListener
 import com.google.firebase.database.ktx.database
+import com.google.firebase.database.ktx.getValue
 import com.google.firebase.ktx.Firebase
 
 private const val TAG = "DataRepository"
@@ -13,8 +19,11 @@ class DataRepository private constructor(context: Context) {
 
     // Get a reference to the Firebase Realtime Database for this project.
     private val database = Firebase.database.reference
+    private val userAuth = Firebase.auth.currentUser
+
 
     // QUERIES
+
 
     // Write a new User.
     fun writeUser(user: User, uid: String) {
@@ -24,6 +33,11 @@ class DataRepository private constructor(context: Context) {
     fun writeGoal(goal: Goal, uid:String) {
         Log.d(TAG, "writing new goal at " + uid)
         database.child("users").child(uid).child("goal").setValue(goal)
+    }
+
+    fun updatePassword(uid: String, newPassword: String){
+        userAuth!!.updatePassword(newPassword)
+        //TODO add on completion listeners
     }
 
     companion object {
--- app/src/main/java/com/bignerdranch/android/wellnesspal/ui/profile/ProfileViewModel.kt
+++ app/src/main/java/com/bignerdranch/android/wellnesspal/ui/profile/ProfileViewModel.kt
@@ -3,11 +3,60 @@ package com.bignerdranch.android.wellnesspal.ui.profile
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.bignerdranch.android.wellnesspal.DataRepository
+import com.bignerdranch.android.wellnesspal.models.User
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.ValueEventListener
+import com.google.firebase.database.ktx.getValue
 
 class ProfileViewModel : ViewModel() {
+    private val dataRepository = DataRepository.get()
+    private var auth = FirebaseAuth.getInstance()
 
-    private val _text = MutableLiveData<String>().apply {
-        value = "This is Profile Fragment"
+    private val _userData = MutableLiveData<User>()
+    val userData: LiveData<User> = _userData
+
+
+
+    fun addUserEventListener(userReference: DatabaseReference){
+        val userListener = object : ValueEventListener {
+            override fun onDataChange(dataSnapshot: DataSnapshot){
+                val user = dataSnapshot.getValue<User>()
+
+                //update the liveData with the new value from the listener
+                user?.let {
+                    _userData.value = it
+                }
+            }
+            override fun onCancelled(error: DatabaseError) {
+                TODO("Not yet implemented")
+            }
+        }
+        userReference.addValueEventListener(userListener)
+
     }
-    val text: LiveData<String> = _text
+
+    /*
+    Used when a user tries to reset their password
+    Compares their current password in the db
+        with the value they entered as their old password
+    If the passwords match, return true
+    If the passwords don't match, return false
+     */
+    private fun passwordCompare(enteredPassword: String): Boolean{
+        if (_userData.value!!.hashedPass == enteredPassword) {
+            return true
+        }
+        return false
+    }
+
+    fun resetPassword(oldPassword: String, newPassword: String, reEnteredNewPass: String){
+        if (passwordCompare(oldPassword)){
+            dataRepository.updatePassword(auth.currentUser!!.uid, newPassword)
+        }
+    }
+
 }
