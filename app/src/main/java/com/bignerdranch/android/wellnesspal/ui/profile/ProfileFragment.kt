c8b96fef6939ba9fec9efeeef00df9cd080cd1ac
Emma Ryan <ryan.1778@buckeyemail.osu.edu>
2023-11-18 09:50:34 -0500

    implemented in profile fragment

--- app/src/main/java/com/bignerdranch/android/wellnesspal/ui/profile/ProfileFragment.kt
+++ app/src/main/java/com/bignerdranch/android/wellnesspal/ui/profile/ProfileFragment.kt
@@ -1,6 +1,10 @@
 package com.bignerdranch.android.wellnesspal.ui.profile
 
+import android.content.Context
 import android.content.Intent
+import android.net.ConnectivityManager
+import android.net.NetworkCapabilities
+import android.os.Build
 import androidx.lifecycle.ViewModelProvider
 import android.os.Bundle
 import android.util.Log
@@ -79,62 +83,93 @@ class ProfileFragment : Fragment() {
         binding.apply {
             // when user signs out, return to the initial AuthActivity screen
             buttonSignOut.setOnClickListener {
-                auth.signOut()
-                if (auth.currentUser == null) {
-                    startActivity(Intent(activity, AuthActivity::class.java))
-                } else {
-                    Toast.makeText(context, "Error Signing Out", Toast.LENGTH_SHORT)
+                if (checkForInternet(view.context)) {
+                    auth.signOut()
+                    if (auth.currentUser == null) {
+                        startActivity(Intent(activity, AuthActivity::class.java))
+                    } else {
+                        Toast.makeText(context, "Error Signing Out", Toast.LENGTH_SHORT)
+                    }
+                }else{
+                    Toast.makeText(view.context, "No Internet Connection", Toast.LENGTH_SHORT).show()
                 }
             }
 
             buttonResetPassword.setOnClickListener {
-                auth.sendPasswordResetEmail(auth.currentUser!!.email.toString())
-                    .addOnCompleteListener { task ->
-                        if (task.isSuccessful) {
-                            Log.d(TAG, "Email Send Success")
-                            Toast.makeText(context, "Password Reset Email Sent", Toast.LENGTH_SHORT)
-                                .show()
-                        } else {
-                            Log.d(TAG, "Email Send Failure")
-                            Toast.makeText(
-                                context,
-                                "Unable to Send Password Reset Email",
-                                Toast.LENGTH_SHORT
-                            ).show()
+                if(checkForInternet(view.context)) {
+                    auth.sendPasswordResetEmail(auth.currentUser!!.email.toString())
+                        .addOnCompleteListener { task ->
+                            if (task.isSuccessful) {
+                                Log.d(TAG, "Email Send Success")
+                                Toast.makeText(
+                                    context,
+                                    "Password Reset Email Sent",
+                                    Toast.LENGTH_SHORT
+                                )
+                                    .show()
+                            } else {
+                                Log.d(TAG, "Email Send Failure")
+                                Toast.makeText(
+                                    context,
+                                    "Unable to Send Password Reset Email",
+                                    Toast.LENGTH_SHORT
+                                ).show()
+                            }
                         }
-                    }
+                }else{
+                    Toast.makeText(view.context, "No Internet Connection", Toast.LENGTH_SHORT).show()
+                }
             }
                 buttonDeleteAccount.setOnClickListener {
                     // first reauthenticate user TODO: ask user to re-enter credentials
                     // auth.currentUser.reauthenticate(EmailAuthProvider.getCredential(auth.currentUser.email, auth.currentUser.pa))
                     // first delete user entry from database
-                    deleteAccount()
+                    if(checkForInternet(view.context)) {
+                        deleteAccount()
+                    }else{
+                        Toast.makeText(view.context, "No Internet Connection", Toast.LENGTH_SHORT).show()
+                    }
                 }
                 // Update goals based on user input.
                 buttonChangeMeals.setOnClickListener {
                     val text = editTextFieldChangeMeals.text.toString()
-                    if (text=="") {
-                        Toast.makeText(context, R.string.empty_field_err, Toast.LENGTH_LONG).show()
-                    } else {
-                        profileViewModel.updateGoal(text, "eat")
-                    }
+                    if (checkForInternet(view.context)) {
+                        if (text == "") {
+                            Toast.makeText(context, R.string.empty_field_err, Toast.LENGTH_LONG)
+                                .show()
+                        } else {
+                            profileViewModel.updateGoal(text, "eat")
+                        }
 
-                    editTextFieldChangeMeals.setText("")
+                        editTextFieldChangeMeals.setText("")
+                    }else{
+                        Toast.makeText(view.context, "No Internet Connection", Toast.LENGTH_SHORT).show()
+                    }
                 }
                 buttonChangeWater.setOnClickListener {
                     val text = editTextFieldChangeWater.text.toString()
-                    if (text=="") {
-                        Toast.makeText(context, R.string.empty_field_err, Toast.LENGTH_LONG).show()
-                    } else {
-                        profileViewModel.updateGoal(text, "water")
+                    if(checkForInternet(view.context)) {
+                        if (text == "") {
+                            Toast.makeText(context, R.string.empty_field_err, Toast.LENGTH_LONG)
+                                .show()
+                        } else {
+                            profileViewModel.updateGoal(text, "water")
+                        }
+                    }else{
+                        Toast.makeText(view.context, "No Internet Connection", Toast.LENGTH_SHORT).show()
                     }
                 }
                 buttonChangeSleep.setOnClickListener {
                     val text = editTextFieldChangeSleep.text.toString()
-                    if (text=="") {
-                        Toast.makeText(context, R.string.empty_field_err, Toast.LENGTH_LONG).show()
-                    } else {
-                        profileViewModel.updateGoal(text, "sleep")
+                    if (checkForInternet(view.context)) {
+                        if (text == "") {
+                            Toast.makeText(context, R.string.empty_field_err, Toast.LENGTH_LONG)
+                                .show()
+                        } else {
+                            profileViewModel.updateGoal(text, "sleep")
+                        }
+                    }else{
+                        Toast.makeText(view.context, "No Internet Connection", Toast.LENGTH_SHORT).show()
                     }
                 }
             }
@@ -161,6 +196,25 @@ class ProfileFragment : Fragment() {
     override fun onStop() {
         super.onStop()
         gradPetsQuery.removeEventListener(profileViewModel.gradPetCountListener)
+    }
+
+    //Code from geeks for geeks geeksforgeeks.com
+    private fun checkForInternet(context: Context): Boolean{
+        val connectivityManager = context.getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M){
+            val network = connectivityManager.activeNetwork?:return false
+            val activeNetwork = connectivityManager.getNetworkCapabilities(network)?:return false
+            return when {
+                activeNetwork.hasTransport(NetworkCapabilities.TRANSPORT_WIFI) -> true
+                activeNetwork.hasTransport(NetworkCapabilities.TRANSPORT_CELLULAR) -> true
+                else -> false
+            }
+        }else{
+            @Suppress("DEPRECATION") val networkInfo = connectivityManager.activeNetworkInfo?: return false
+            @Suppress("DEPRECATION")
+            return networkInfo.isConnected
+        }
     }
 }
 
