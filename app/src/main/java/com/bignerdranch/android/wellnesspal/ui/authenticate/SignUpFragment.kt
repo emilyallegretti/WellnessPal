dc5f193e034c1cc3d8532e0416e5f40a907b6a12
Emma Ryan <ryan.1778@buckeyemail.osu.edu>
2023-11-17 20:55:14 -0500

    implemented in sign up fragment

--- app/src/main/java/com/bignerdranch/android/wellnesspal/ui/authenticate/SignUpFragment.kt
+++ app/src/main/java/com/bignerdranch/android/wellnesspal/ui/authenticate/SignUpFragment.kt
@@ -1,6 +1,10 @@
 package com.bignerdranch.android.wellnesspal.ui.authenticate
 
+import android.content.Context
 import android.content.Intent
+import android.net.ConnectivityManager
+import android.net.NetworkCapabilities
+import android.os.Build
 import android.os.Bundle
 import android.text.TextUtils
 import android.util.Log
@@ -43,8 +47,19 @@ class SignUpFragment: Fragment() {
         super.onViewCreated(view, savedInstanceState)
         binding.apply {
             buttonSignUp.setOnClickListener {
-                signUp(fieldEmail.text.toString(), fieldPassword.text.toString(), fieldFname.text.toString(), fieldLname.text.toString(),
-                    fieldWaterGoal.text.toString(), fieldFoodGoal.text.toString(), fieldSleepGoal.text.toString())
+                if (checkForInternet(view.context)) {
+                    signUp(
+                        fieldEmail.text.toString(),
+                        fieldPassword.text.toString(),
+                        fieldFname.text.toString(),
+                        fieldLname.text.toString(),
+                        fieldWaterGoal.text.toString(),
+                        fieldFoodGoal.text.toString(),
+                        fieldSleepGoal.text.toString()
+                    )
+                }else{
+                    Toast.makeText(view.context, "No Internet Connection", Toast.LENGTH_SHORT).show()
+                }
             }
         }
 
@@ -111,6 +126,24 @@ class SignUpFragment: Fragment() {
                     Toast.LENGTH_SHORT).show()
             }
 
+    }
+    //Code from geeks for geeks geeksforgeeks.com
+    private fun checkForInternet(context: Context): Boolean{
+        val connectivityManager = context.getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M){
+            val network = connectivityManager.activeNetwork?:return false
+            val activeNetwork = connectivityManager.getNetworkCapabilities(network)?:return false
+            return when {
+                activeNetwork.hasTransport(NetworkCapabilities.TRANSPORT_WIFI) -> true
+                activeNetwork.hasTransport(NetworkCapabilities.TRANSPORT_CELLULAR) -> true
+                else -> false
+            }
+        }else{
+            @Suppress("DEPRECATION") val networkInfo = connectivityManager.activeNetworkInfo?: return false
+            @Suppress("DEPRECATION")
+            return networkInfo.isConnected
+        }
     }
 
 }
